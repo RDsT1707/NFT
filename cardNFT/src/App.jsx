eb561b62826c22d62c7a6b2c97b9373f9f8e0a9a
rayan <desmetrayan621@gmail.com>
2025-05-07 15:06:59 +0200

    3

--- cardNFT/src/App.jsx
+++ cardNFT/src/App.jsx
@@ -1,13 +1,28 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="nft-container">
+      <div className="nft-card">
+        {/* NFT Image avec overlay */}
+        <div className="nft-image-wrapper">
+          <img
+            src="/src/assets/img/image-equilibrium.jpg"
+            alt="Equilibrium #3429"
+            className="nft-image"
+          />
+          <div className="image-overlay">
+            <img
+              src="/src/assets/img/icon-view.svg"
+              alt="Equilibrium #3429"
+              className="view-icon"
+            />
+          </div>
+        </div>
+        {/* NFT Details */}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
