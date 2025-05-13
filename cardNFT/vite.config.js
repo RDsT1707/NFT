363f7ad4733a76c414f5d970a08b23eea300f68a
rayan <desmetrayan621@gmail.com>
2025-05-13 15:15:59 +0200

    d2

--- cardNFT/vite.config.js
+++ cardNFT/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NFT/', // ← ici ! Le nom de ton repo GitHub
+  base: '/NFT/',
+  define: {
+    'process.env.NODE_ENV': '"development"',
+  },
 })
